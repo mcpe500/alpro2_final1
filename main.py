f0c45f39b2bb7265d363e56bbf5bc1f02fee5147
mcpe500 <68177813+mcpe500@users.noreply.github.com>
2023-06-01 22:20:09 +0700

    p3

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import generateMap
 import pathfinding
 
 map,state = read_file.readMap("map")
+result = {}
 map = generateMap.randomMap(map, 0, 0)
-
 if not state:
     map = generateMap.newMaze(map)
 path,history = pathfinding.findpath(map)
 map = pathfinding.finalMaze(map,path)
 history.append(map)
-for i in map:
-    print(i)
+# for i in map:
+#     print(i)
 
-for i in history:
-    print(i)
+# for i in history:
+#     print(i)
 import visualize
 visualize.animate(history)
 
