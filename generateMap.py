092db6faa0fb90a3da701cd226585fe6ab48ce2e
mcpe500 <68177813+mcpe500@users.noreply.github.com>
2023-06-06 14:36:27 +0700

    update generatemap

--- generateMap.py
+++ generateMap.py
@@ -6,48 +6,48 @@ sys.setrecursionlimit(100000)
 def createEmptyMap(width, height):
     return [[1] * width for _ in range(height)]
 
-def isAllVisited(visited):
-    for i in visited:
-        for j in i:
-            if not j:
-                return False
-    return True
+# def isAllVisited(visited):
+#     for i in visited:
+#         for j in i:
+#             if not j:
+#                 return False
+#     return True
 
-def isStuck(visited, yx, wall):
-    temp = visited.copy()
-    for i in wall:
-        if (yx[0] + i[0] < 0 or yx[0] + i[0] >= len(visited) or yx[1] + i[1] < 0 or yx[1] + i[1] >= len(visited[0])):
-            continue
-        else:
-            temp[yx[0] + i[0]][yx[1] + i[1]] = True
+# def isStuck(visited, yx, wall):
+#     temp = visited.copy()
+#     for i in wall:
+#         if (yx[0] + i[0] < 0 or yx[0] + i[0] >= len(visited) or yx[1] + i[1] < 0 or yx[1] + i[1] >= len(visited[0])):
+#             continue
+#         else:
+#             temp[yx[0] + i[0]][yx[1] + i[1]] = True
     
-    if not isAllVisited(temp):
-        if yx[0] == 0:
-            if not temp[yx[0]+1][yx[1]]:
-                return True
-        elif yx[0] == len(temp)-1:
-            if not temp[yx[0]-1][yx[1]]:
-                return True
-        elif yx[1] == 0:
-            if not temp[yx[0]][yx[1]+1]:
-                return True
-        elif yx[1] == len(temp[0])-1:
-            if not temp[yx[0]][yx[1]-1]:
-                return True
-        else:
-            if not temp[yx[0]+1][yx[1]] and not temp[yx[0]-1][yx[1]] and not temp[yx[0]][yx[1]+1] and not temp[yx[0]][yx[1]-1]:
-                return True
-    return False
+#     if not isAllVisited(temp):
+#         if yx[0] == 0:
+#             if not temp[yx[0]+1][yx[1]]:
+#                 return True
+#         elif yx[0] == len(temp)-1:
+#             if not temp[yx[0]-1][yx[1]]:
+#                 return True
+#         elif yx[1] == 0:
+#             if not temp[yx[0]][yx[1]+1]:
+#                 return True
+#         elif yx[1] == len(temp[0])-1:
+#             if not temp[yx[0]][yx[1]-1]:
+#                 return True
+#         else:
+#             if not temp[yx[0]+1][yx[1]] and not temp[yx[0]-1][yx[1]] and not temp[yx[0]][yx[1]+1] and not temp[yx[0]][yx[1]-1]:
+#                 return True
+#     return False
 
-def depth_first_search_map(x, y, maze):
-    maze[y][x] = 0
-    possible = [[0, 2], [2, 0], [0, -2], [-2, 0]]
-    random.shuffle(possible)
-    for x1, y1 in possible:
-        lx1, ly1 = x + x1, y + y1
-        if 0 <= lx1 < len(maze[0]) and 0 <= ly1 < len(maze) and maze[ly1][lx1] == 1:
-            maze[y + y1 // 2][x + x1 // 2] = 0
-            depth_first_search_map(lx1, ly1, maze)
+# def depth_first_search_map(x, y, maze):
+#     maze[y][x] = 0
+#     possible = [[0, 2], [2, 0], [0, -2], [-2, 0]]
+#     random.shuffle(possible)
+#     for x1, y1 in possible:
+#         lx1, ly1 = x + x1, y + y1
+#         if 0 <= lx1 < len(maze[0]) and 0 <= ly1 < len(maze) and maze[ly1][lx1] == 1:
+#             maze[y + y1 // 2][x + x1 // 2] = 0
+#             depth_first_search_map(lx1, ly1, maze)
 
 def randomMap(maze, x, y,visited=None):
     maze[y][x] = 0
@@ -64,7 +64,7 @@ def randomMap(maze, x, y,visited=None):
         next_moveY = y + moveY
         if (next_moveX >= 0 and next_moveX < len(maze[0]) and next_moveY >= 0 and next_moveY < len(maze)):
             if maze[next_moveY][next_moveX] == 1 and not visited[next_moveY][next_moveX]:
-                maze[y+moveY//2][x+moveX//2] = 0
+                maze[y+moveY][x+moveX] = 0
                 wall = []
                 if(moveX,moveY) == moves[0]:
                     wall = cover[0]
@@ -108,23 +108,23 @@ def newMaze(maze, trapCount, lavaCount):
             y,x = random.randint(0,len(maze)-1),random.randint(0,len(maze[0])-1)
         nMaze[y][x] = 6
 
-    def validLava(maze, x, y):
-        empty = 0
-        if x-1>=0 and maze[y][x-1] == 0:
-            empty += 1
-        if x+1<len(maze[0]) and maze[y][x+1] == 0:
-            empty += 1
-        if y-1>=0 and maze[y-1][x] == 0:
-            empty += 1
-        if y+1<len(maze) and maze[y+1][x] == 0:
-            empty += 1
-        return ((empty>=2) and maze[y][x] == 1)
+    # def validLava(maze, x, y):
+    #     empty = 0
+    #     if x-1>=0 and maze[y][x-1] == 0:
+    #         empty += 1
+    #     if x+1<len(maze[0]) and maze[y][x+1] == 0:
+    #         empty += 1
+    #     if y-1>=0 and maze[y-1][x] == 0:
+    #         empty += 1
+    #     if y+1<len(maze) and maze[y+1][x] == 0:
+    #         empty += 1
+    #     return ((empty>=2) and maze[y][x] == 1)
 
-    for i in range(0, lavaCount):
-        y,x = random.randint(0,len(maze)-1),random.randint(0,len(maze[0])-1)
-        while not validLava(maze, x, y):
-            y,x = random.randint(0,len(maze)-1),random.randint(0,len(maze[0])-1)
-        nMaze[y][x] = 7
+    # for i in range(0, lavaCount):
+    #     y,x = random.randint(0,len(maze)-1),random.randint(0,len(maze[0])-1)
+    #     while not validLava(maze, x, y):
+    #         y,x = random.randint(0,len(maze)-1),random.randint(0,len(maze[0])-1)
+    #     nMaze[y][x] = 7
 
     return nMaze
 
